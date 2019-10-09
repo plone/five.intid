89e2f7b3f1c4275c3ccfd5fb8560e14032b20809
Maurits van Rees <maurits@vanrees.org>
2019-10-09 22:06:06 +0200

    Added development status 5 production/stable to classifiers.
    [ci skip]

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
     description="Zope2 support for zope.intid",
     long_description=longdescripton,
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Zope2",
         "Framework :: Zope :: 4",
         "License :: OSI Approved :: Zope Public License",
