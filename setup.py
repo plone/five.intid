69a0d73b2722e3622724728b8f126d45b8bc30d0
Maurits van Rees <maurits@vanrees.org>
2020-05-06 23:25:52 +0200

    Back to development: 1.2.7
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 import os.path
 
-version = '1.2.6'
+version = '1.2.7.dev0'
 
 longdescripton = open("README.rst").read() + "\n"
 longdescripton += open(os.path.join("five", "intid", "README.rst")).read()
