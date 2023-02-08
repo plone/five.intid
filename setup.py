2c0b714a76d7f74e67fcb52b498d12e31779b1fb
Gil Forcada Codinachs <gil.gnome@gmail.com>
2023-02-08 18:46:40 +0100

    chore: pyroma

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     namespace_packages=["five"],
     include_package_data=True,
     zip_safe=False,
+    python_requires=">=3.8",
     install_requires=[
         "Acquisition>=4.0.1",
         "setuptools",
