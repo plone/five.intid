86647519760893387041288f3404ae71c5c47e5c
Michael Howitz <icemac@gmx.net>
2025-02-12 15:40:26 +0100

    Simplify more.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from setuptools import find_namespace_packages
 from setuptools import setup
 
 
@@ -37,7 +36,6 @@ setup(
     author_email="whit@openplans.org",
     license="ZPL",
     url="https://github.com/plone/five.intid",
-    packages=find_namespace_packages(),
     include_package_data=True,
     zip_safe=False,
     python_requires=">=3.8",
